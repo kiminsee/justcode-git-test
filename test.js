d9a9fcfd75684819bf3f43cd5f8c5846c1fb6439
kiminSong <kiminsee22@gmail.com>
2022-10-16 21:29:54 +0900

    짝수 구하는 함수

---
+++ test.js
@@ -0,0 +1,12 @@
+function arrNumber() {
+  let arr = [];
+  for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+arrNumber();
+console.log(arrNumber());
